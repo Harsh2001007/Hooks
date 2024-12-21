6086d7d1abde0e1efe34da5f8c3be2f16641a1b2
Harsh <hs5686584@gmail.com>
2024-12-22 01:07:28 +0530

    useEffect done

--- useEffect-hook/src/App.jsx
+++ useEffect-hook/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resourceType, setResourceType] = useState("Posts");
+  const [testState, setTestState] = useState("");
+  const [apiData, setApiData] = useState([]);
+  const [windowWidth, setWindowwidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    console.log("Bye world");
+  }, [testState]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((resp) => resp.json())
+      .then((json) => setApiData(json));
+  }, [resourceType]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const handleResize = () => {
+    setWindowwidth(window.innerWidth);
+    console.log("changing");
+  };
   return (
     <>
+      <div>{windowWidth}</div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setResourceType("Posts")}>Posts</button>
+        <button onClick={() => setResourceType("Users")}>Users</button>
+        <button onClick={() => setResourceType("Comments")}>Comments</button>
+        <button onClick={() => setTestState("Like")}>Like</button>
+        <button onClick={() => setTestState("Dislike")}>Dislike</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{resourceType}</h1>
+      <h1>{testState}</h1>
+      {apiData.map((item, index) => (
+        <pre key={index}>{JSON.stringify(item)}</pre>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
